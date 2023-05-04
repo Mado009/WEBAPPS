d97d3fc859f926105def71cde84528b94aad9c91
Mado009 <94806777+Mado009@users.noreply.github.com>
2023-05-04 20:48:03 +0000

    Done

--- project2/src/Components/Property.js
+++ project2/src/Components/Property.js
@@ -1,8 +1,69 @@
+import CardItem from "./CardItem"
+import "./CardItem.css"
+
+CardItem
 const Property = () => {
   return (
     <div className="property">
-    <h1>Propertys</h1>
-    <p>Gjeni shtepin e enderrave tuaja</p>
+      <h1>Properties</h1>
+      <p>Gjeni shtepin e enderrave tuaja</p>
+      <div className='cards__container'>
+                <div className='cards__wrapper'>
+                    <ul className='cards__items'>
+                        <CardItem
+                            src="/images/img.jpg"
+                            text="Vile"
+                            label='Shitet'
+                            path='/services'
+                        />
+
+                        <CardItem
+                            src="/images/img.jpg"
+                            text="Apartament 1+1"
+                        label='Qira'
+                            path='/services'
+                        />
+                        <CardItem
+                            src="/images/img-5.jpg"
+                            text="Apartament 2+1"
+                            label='Qira'
+                            path='/services'
+                        />
+                        <CardItem
+                            src="/images/img-8.jpg"
+                            text="Apartament "
+                            label='Shitet'
+                            path='/services'
+                        />
+                        <CardItem
+                            src="/images/img.jpg"
+                            text="Vile"
+                            label='Qira'
+                            path='/services'
+                        />
+
+                        <CardItem
+                            src="/images/img.jpg"
+                            text="Apartament 2+1"
+                            label='Shitet'
+                            path='/services'
+                        />
+                        <CardItem
+                            src="/images/img-5.jpg"
+                            text="Biznes"
+                            label='Qira'
+                            path='/services'
+                        />
+                        <CardItem
+                            src="/images/img-8.jpg"
+                            text="Apartament 3+2"
+                            label='Shitet'
+                            path='/services'
+                        />
+                    </ul>
+
+                </div>
+            </div>
     </div>
   )
 }
